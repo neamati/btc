d6818c8d17354aac1415dc9690fd9748864ca331
neamati <neamati@tiyansoft.com>
2025-05-07 03:40:32 +0430

    fixing the spinning error on the invoice page

--- btc.php
+++ btc.php
@@ -176,8 +176,7 @@ function btc_link($params)
         </div>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>';
 
     $htmlOutput .= '<link rel="stylesheet" type="text/css" href="modules/gateways/btc/assets/btc-styles.css">';
